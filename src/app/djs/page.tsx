3ae48715b42f86cf8396db0cbb92b076ef838441
Christopher Norman <chris@artofdigging.com>
2024-07-09 23:30:06 -0700

    Adding dj page

--- src/app/djs/page.tsx
+++ src/app/djs/page.tsx
@@ -2,6 +2,8 @@ import { PageHeading } from '@/components/page-heading/page-heading'
 import styles from './djs.module.css'
 import { DJS } from '@/components/djs/djs'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   return (
     <div className={styles.container}>
